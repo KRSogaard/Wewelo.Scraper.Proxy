a598d44d70c59beb75c8977fa27e04bfbd5b2e26
Kasper Rune Søgaard <kasper@sogaard.us>
2017-05-28 13:18:32 -0700

    Inital setup

---
+++ Wewelo.Scraper.Proxy.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace Wewelo.Scraper.Proxy.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
